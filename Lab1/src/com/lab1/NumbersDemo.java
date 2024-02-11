2dee614051a7b0603c59988c3df8826dfb520f31
Pavan <Pavan@DESKTOP-ARP6G1H.mynetworksettings.com>
2024-02-11 03:40:28 -0500

    first commit

---
+++ Lab1/src/com/lab1/NumbersDemo.java
@@ -0,0 +1,30 @@
+package com.lab1;
+
+public class NumbersDemo {
+
+	public static void displayTwiceTheNumber(int number) {
+		
+		System.out.println("Twice the number " + number + " is: " + (number * 2));
+	
+	}
+	public static void displayNumberPlusFive(int number) {
+        System.out.println("Number " + number + " plus five is: " + (number + 5));
+    }
+
+    public static void displayNumberSquared(int number) {
+        System.out.println("Number " + number + " squared is: " + (number * number));
+    }
+		public static void main(String[] args) {
+  int a = 5;
+  int b=6;
+  displayTwiceTheNumber(a);
+  displayNumberPlusFive(a);
+  displayNumberSquared(a);
+
+ 
+  displayTwiceTheNumber(b);
+  displayNumberPlusFive(b);
+  displayNumberSquared(b);
+	} 
+
+}
